57b42a94cf8749124e1167a1ae8fa0e0cb5023c1
Gabriel Vladoiu <gabriel.vladoiu@connatix.com>
2025-08-18 13:33:31 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+using HtmlAgilityPack;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter url:");
+        string url = Console.ReadLine();
+        Console.WriteLine("enter tag:");
+        string tag = Console.ReadLine();
+        //Console.WriteLine($"//{tag}");
+        Thread thread=new Thread(()=>CrawlSite(url,tag));
+        thread.Start();
+        thread.Join();
+    }
+
+    static void CrawlSite(string url, string tag)
+    {
+        try
+        {   
+            using (HttpClient client = new HttpClient())
+            {
+                var html = client.GetStringAsync(url).Result;
+                Console.WriteLine(html);
+                HtmlDocument document = new HtmlDocument();
+                document.LoadHtml(html);
+                var nodes = document.DocumentNode.SelectNodes($"//{tag}");
+            
+                int count = nodes?.Count ?? 0;
+                Console.WriteLine($"tag <{tag}>  appears {count} times on {url}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+    }
+}
